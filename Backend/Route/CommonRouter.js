83f71aac58a20d3107366f5d692fd154f4c66d9e
Hari <snshari80@gmail.com>
2024-11-10 12:44:56 +0530

    Routes

--- Backend/Route/CommonRouter.js
+++ Backend/Route/CommonRouter.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/login",(req,res)=>{
-   return res.status(200).json({data:'Login Call'});
+router.get("/",(req,res)=>{
+   return res.status(200).json({ message: 'success' });
 })
 
 export default router;
